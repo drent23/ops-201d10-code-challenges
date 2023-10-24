b48ff26ddc752f2be3bb7c6c72d3cdeaa8672332
David <drent23>
2023-10-24 17:50:31 -0500

    first commit of ops challenge

---
+++ helloworld.sh
@@ -0,0 +1,16 @@
+# Ops Challenge: Class 02
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       David Renteria
+# Date of latest revision:      10/24/2023
+# Purpose:                      Print to screen
+
+# Declaration of variables
+helloWorld="I am working perfectly!"
+# Declaration of functions
+echo $helloWorld
+
+# Main
+
+# End
