b8e2edf3e47c624b65e4c19183f5ce057729aebf
David <drent23>
2023-10-29 14:30:09 -0500

    ops challenge 05 completek

---
+++ ops_challenge_class05.sh
@@ -0,0 +1,23 @@
+# Ops Challenge: Class 04
+#!/bin/bash
+# Script Name:                  Bash Loops
+# Author:                       David Renteria
+# Date of latest revision:      10/27/2023
+# Purpose:                      Script that identifies and kills a process and uses a loop
+
+# Declaration of variables
+count=1
+# Declaration of functions
+
+# Define an array
+
+# Main
+while [ $count -gt 0 ]; do
+    ps aux
+    read -p "Please enter the PID you would like to kill: " pid
+    kill $pid
+    echo "Your selected process, $pid, was terminated."
+done
+# Call the function
+
+# End
