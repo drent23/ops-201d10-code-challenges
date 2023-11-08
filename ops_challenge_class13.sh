c82ff6b69bf296e26b0600a0334327f1a01db358
David <drent23>
2023-11-08 15:37:21 -0600

    ops challenge class 13 done

---
+++ ops_challenge_class13.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# install whois
+sudo apt-get install whois
+# ask user for domain
+read -p "Please enter a domain you would like to visit, such as google.com: " user_input
+# name the txt file to whatever domain user enters
+domain_info="$user_input"_info
+# get domain info
+get_domain_info() {
+    domain="$1"
+    echo "Whois for $domain: "
+    whois "$domain" > "$domain_info.txt"
+    echo "Dig for $domain: "
+    dig "domain" >> "$domain_info.txt"
+    echo "Host for $domain: "
+    host "$domain" >> "$domain_info.txt"
+    echo "NSLookup for $domain: "
+    nslookup "$domain" >> "$domain_info.txt"
+    echo "The domain info has been saved to $domain_info.txt"
+}
+# call the function
+get_domain_info "$user_input"
+# open txt file
+gedit "$domain_info.txt"
